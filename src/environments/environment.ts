43382fa8cc8d02a2f3677af23cc90de95a68d4e6
BryanHernz <126428695+BryanHernz@users.noreply.github.com>
2023-12-20 13:52:52 -0300

    project upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment= {
     production: true, 
-    //urlBase:'http://localhost:8000/api',
     urlBase:'https://subelotodo-api-grw4z.ondigitalocean.app/',
-    imagesUrl:'https://subelotodo-api-grw4z.ondigitalocean.app/'
+    imagesUrl:'https://subelotodo-api-grw4z.ondigitalocean.app/',
 };
+//urlBase:'http://localhost:8000/api',
