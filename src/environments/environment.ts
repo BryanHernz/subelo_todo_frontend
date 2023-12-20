cad721188a09c891edf621327d47f99f886d814d
BryanHernz <126428695+BryanHernz@users.noreply.github.com>
2023-12-20 10:19:15 -0300

    project upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment= {
     production: true, 
-    urlBase:'http://localhost:8000/api',
+    //urlBase:'http://localhost:8000/api',
+    urlBase:'https://subelotodo-api-grw4z.ondigitalocean.app/',
+    imagesUrl:'https://subelotodo-api-grw4z.ondigitalocean.app/'
 };
